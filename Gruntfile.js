80fc296c6a8d1184d6cd3110f675b8a78c57bebb
Aditya Bhatt <aditya.bhatt@kolabsys.com>
2015-07-13 17:25:58 +0200

    Make `grunt build` work with dots in dependency names

--- Gruntfile.js
+++ Gruntfile.js
@@ -234,7 +234,7 @@ module.exports = function (grunt) {
     },
 
     // Renames files for browser caching purposes
-    rev: {
+    filerev: {
       dist: {
         files: {
           src: [
@@ -684,7 +684,7 @@ module.exports = function (grunt) {
     'cdnify',
     'cssmin',
     'uglify',
-    'rev',
+    'filerev',
     'usemin'
   ]);
 
