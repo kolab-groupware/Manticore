f9e7612881eab15beff451e755e4f9683e9ea1ec
Aditya Bhatt <aditya.bhatt@kolabsys.com>
2015-07-15 14:03:55 +0200

    Use grunt-filerev in the "new" way

--- Gruntfile.js
+++ Gruntfile.js
@@ -235,14 +235,12 @@ module.exports = function (grunt) {
     // Renames files for browser caching purposes
     filerev: {
       dist: {
-        files: {
-          src: [
-            '<%= yeoman.dist %>/public/{,*/}*.js',
-            '<%= yeoman.dist %>/public/{,*/}*.css',
-            '<%= yeoman.dist %>/public/assets/images/{,*/}*.{png,jpg,jpeg,gif,webp,svg}',
-            '<%= yeoman.dist %>/public/assets/fonts/*'
-          ]
-        }
+        src: [
+          '<%= yeoman.dist %>/public/{,*/}*.js',
+          '<%= yeoman.dist %>/public/{,*/}*.css',
+          '<%= yeoman.dist %>/public/assets/images/{,*/}*.{png,jpg,jpeg,gif,webp,svg}',
+          '<%= yeoman.dist %>/public/assets/fonts/*'
+        ]
       }
     },
 
