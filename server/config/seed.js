89b1526ec67a9bd8eec392862a7612a0da677bed
Aditya Bhatt <aditya.bhatt@kolabsys.com>
2015-07-16 14:27:52 +0200

    Create admin account when there is none

--- server/config/seed.js
+++ server/config/seed.js
@@ -1,5 +1,5 @@
 /**
- * Populate DB with sample data on server start
+ * Populate DB with sample data on first run
  * to disable, edit config/environment/index.js, and set `seedDB: false`
  */
 
@@ -9,25 +9,17 @@ var Document = require('../api/document/document.model').Document;
 var DocumentChunk = require('../api/document/document.model').DocumentChunk;
 var User = require('../api/user/user.model');
 
-DocumentChunk.find({}).remove(function() {
-    Document.find({}).remove(function() {
-    });
-});
-
-User.find({}).remove(function() {
-  User.create({
-    provider: 'local',
-    name: 'Test User',
-    email: 'test@test.com',
-    password: 'test'
-  }, {
-    provider: 'local',
-    role: 'admin',
-    name: 'Admin',
-    email: 'admin@admin.com',
-    password: 'admin'
-  }, function() {
-      console.log('finished populating users');
+// Create an administrator if there isn't any
+User.findOne({role: 'admin'}, function (err, admin) {
+    if (!admin) {
+        User.create({
+            provider: 'local',
+            role: 'admin',
+            name: 'Administrator',
+            email: 'admin@admin.com',
+            password: 'admin'
+        }, function () {
+            console.log('Created Administrator with username "admin" and password "admin", make sure to change the password ASAP.');
+        });
     }
-  );
 });
