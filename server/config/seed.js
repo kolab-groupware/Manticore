ddc4f9fa71d06b7a0c50ebd91086f45fb9d69ba6
Aditya Bhatt <aditya.bhatt@kolabsys.com>
2015-07-21 00:13:40 +0200

    Add blank template with a sane set of styles

--- server/config/seed.js
+++ server/config/seed.js
@@ -5,9 +5,15 @@
 
 'use strict';
 
+var fs = require('fs');
+var mongoose = require('mongoose');
+var Grid = require('gridfs-stream');
+var gfs = Grid(mongoose.connection.db, mongoose.mongo);
+
 var Document = require('../api/document/document.model').Document;
 var DocumentChunk = require('../api/document/document.model').DocumentChunk;
 var User = require('../api/user/user.model');
+var Template = require('../api/template/template.model');
 
 // Create an administrator if there isn't any
 User.findOne({role: 'admin'}, function (err, admin) {
@@ -20,6 +26,37 @@ User.findOne({role: 'admin'}, function (err, admin) {
             password: 'admin'
         }, function () {
             console.log('Created Administrator with username "admin" and password "admin", make sure to change the password ASAP.');
+        });
+    }
+});
+
+Template.findOne({}, function (err, template) {
+    if (!template) {
+        var templateFileId = new mongoose.Types.ObjectId();
+
+        var newTemplate = new Template({
+            title: 'Text Document',
+            description: 'A blank document with a basic set of styles.',
+            fileId: templateFileId
+        });
+
+        var writeStream = gfs.createWriteStream({
+            _id: templateFileId,
+            filename: 'template.odt',
+            mode: 'w',
+            chunkSize: 1024 * 4,
+            content_type: 'application/vnd.oasis.opendocument.text',
+            root: 'fs'
+        });
+
+        require('fs')
+            .createReadStream(__dirname + '/resources/template.odt')
+            .pipe(writeStream);
+
+        writeStream.on('finish', function () {
+            newTemplate.save(function () {
+                console.log('Create template "Text Document" from fallback configuration.')
+            });
         });
     }
 });
